7f59b9b22d3f352c6f1eb7f2ebfb5fbb638bebf7
“manjeety87” <manjeetyadav41000@gmail.com>
2025-03-23 23:54:59 -0400

    started the project with fetching the books

---
+++ script.js
@@ -0,0 +1,15 @@
+const url =
+  "https://api.freeapi.app/api/v1/public/books?page=1&limit=10&inc=kind%252Cid%252Cetag%252CvolumeInfo&query=tech";
+const options = { method: "GET", headers: { accept: "application/json" } };
+
+const getBooks = async () => {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getBooks();
